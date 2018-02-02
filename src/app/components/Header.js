f9f05aff55dd284e93c00557fb2b3d41744862cc
immacmillan <immacmillan@gmail.com>
2018-02-01 21:25:38 -0500

    Second Commit

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -16,7 +16,7 @@ class Header extends Component {
   render() {
     return (
       <header className="header">
-        <h1>todos</h1>
+        <h1>Imran&#39;s Todos</h1>
         <TodoTextInput
           newTodo
           onSave={this.handleSave}
